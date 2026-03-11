e83aec50afe44a6e5fbd92165999c92e366f17f1
ngthanhhoa0927-crypto <ngthanhhoa0927@gmail.com>
2026-03-11 08:35:40 +0700

    Initial commit: Uploaded unzipped backend code

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import authRouter from './routes/auth.route.js'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.route('/auth', authRouter)
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
+
